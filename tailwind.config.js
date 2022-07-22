480aa630a007389b7e01f2c15b958c7e782dafd2
soplay <tkdgns1341@gmail.com>
2022-07-23 00:23:27 +0900

    Add color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const { gray } = require('tailwindcss/colors');
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: { title: gray[900], main: gray[600] },
+        darkBase: { title: gray[50], main: gray[300] },
+      },
+    },
   },
   plugins: [],
-}
+};
