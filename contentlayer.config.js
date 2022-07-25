b5b96d49548289e54565c665f51fad5994c983bd
soplay <tkdgns1341@gmail.com>
2022-07-25 21:35:44 +0900

    Fix contentlayer config

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,23 +1,31 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+const titleFilter = (props) =>
+  props.replace(/ /g, '+').replace(/([^a-zA-z0-9가-힣-+])/g, '');
+
 export const Post = defineDocumentType(() => ({
   name: 'Post',
   filePathPattern: '**/*.(md|mdx)',
+  contentType: 'mdx',
   fields: {
     title: {
       type: 'string',
       description: 'Title of the post',
       required: true,
     },
-    date: {
-      type: 'date',
-      description: 'Date of the post',
-      required: true,
-    },
     tags: {
       type: 'list',
       of: { type: 'string' },
       description: 'Tags of the post',
-      required: false,
+      required: true,
+    },
+    description: {
+      type: 'string',
+      description: 'Description of the post',
+      required: true,
+    },
+    date: {
+      type: 'date',
+      description: 'Date of the post',
+      required: true,
     },
     serise: {
       type: 'string',
@@ -26,9 +34,13 @@ export const Post = defineDocumentType(() => ({
     },
   },
   computedFields: {
+    fixedTitle: {
+      type: 'string',
+      resolve: (post) => titleFilter(post.title),
+    },
     url: {
       type: 'string',
-      resolve: (post) => `/posts/${post._raw.flattenedPath}`,
+      resolve: (post) => `/posts/${titleFilter(post.title)}`,
     },
   },
 }));
