fa4dd519d43c39889668f91e86e8eca383cb9137
soplay <tkdgns1341@gmail.com>
2022-07-26 21:45:06 +0900

    Fix next-image loader

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
-const { withContentlayer } = require("next-contentlayer");
+const { withContentlayer } = require('next-contentlayer');
 
 module.exports = withContentlayer({
   images: {
     domains: ['localhost', 'avatars.githubusercontent.com'],
+    loader: 'akamai',
+    path: '',
   },
-  reactStrictMode: true,
+
+  // reactStrictMode: true,
 });
