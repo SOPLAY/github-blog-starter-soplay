40fec73eea7bd9f6e3afd48cd1be791a462d912e
soplay <tkdgns1341@gmail.com>
2022-07-28 23:59:35 +0900

    Add search query logic

--- components/common/Input/index.tsx
+++ components/common/Input/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import { HiSearch } from 'react-icons/hi';
 
@@ -6,15 +7,17 @@ interface IInput {
 }
 const Input: React.FC<IInput> = ({ set }) => {
   const [inputValue, setInputValue] = useState('');
+  const router = useRouter();
   return (
     <div className='relative'>
       <input
         type='text'
-        className='w-full h-12 px-4 text-2xl text-black rounded-full bg-base-bg border-[3px] dark:bg-dark-bg placeholder:text-dark-footerText'
+        className='w-full h-12 px-4 text-2xl rounded-full bg-base-bg border-[3px] dark:bg-dark-bg placeholder:text-dark-footerText dark:text-dark-title '
         placeholder='Search...'
         onChange={(e) => {
           setInputValue(e.target.value);
           set(e.target.value);
+          router.push({ query: { search: e.target.value } });
         }}
         value={inputValue}
       />
