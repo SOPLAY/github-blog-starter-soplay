08870e602fc4fd714693f019ffb07880c1f052d9
soplay <tkdgns1341@gmail.com>
2022-08-31 01:24:10 +0900

    Add main pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,58 @@
+import { allPosts, Post } from '@root/.contentlayer/generated';
 import Meta from '@root/components/common/Meta';
+import PostCard from '@root/components/common/PostCard';
+import _ from 'lodash';
 import type { NextPage } from 'next';
+import Link from 'next/link';
+import { useEffect, useRef } from 'react';
+
+const Card = () => {
+  return <div>카드</div>;
+};
+
+const fixFilePath = (str: string, post: Post) => {
+  ['https://', 'http://'];
+  if (
+    str.slice(0, 8).includes('https://') ||
+    str.slice(0, 7).includes('http://')
+  )
+    return str;
+  const fixedUrl = `${post._raw.sourceFileDir}/${str}`.replaceAll('./', '');
+
+  return require(`../posts/${post._raw.sourceFileDir}/${str.replace('./', '')}`)
+    .default.src;
+};
 
 const Home: NextPage = () => {
+  const pageRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  });
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+    <div
+      className='flex flex-col items-center justify-center min-h-screen py-2 pt-32'
+      ref={pageRef}
+    >
       <Meta />
+
+      <div className={'w-[90%] '}>
+        <h1 className={'text-3xl font-bold border-b pb-4 mb-4'}>
+          최신 포스트를 살펴보세요!
+        </h1>
+
+        <div className={'md:w-4/5 mx-auto '}>
+          {_.orderBy(allPosts, ['date'], ['desc']).map((value, index) => {
+            if (index >= 5) return;
+            return (
+              <PostCard
+                key={index}
+                {...value}
+                image={value.image ? fixFilePath(value.image, value) : ''}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
