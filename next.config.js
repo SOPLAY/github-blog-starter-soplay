53f208eefa8bbf055d279e71f26983139b06887a
soplay <tkdgns1341@gmail.com>
2022-08-31 14:33:35 +0900

    Fix header , footer logic , style

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
+const { url } = require('./blog.config');
 const { withContentlayer } = require('next-contentlayer');
 const withMDX = require('@next/mdx')({
   extension: /\.(md|mdx)$/,
 });
+const debug = process.env.NODE_ENV !== 'production';
+const targetUrl = url.replace('//', '').split('/').pop();
+console.log('targetUrl', targetUrl);
 module.exports = withContentlayer(
   withMDX({
+    basePath: targetUrl !== '' ? `/${targetUrl}` : '',
+    assetPrefix: !debug ? targetUrl : '',
     images: {
       domains: ['localhost', 'avatars.githubusercontent.com'],
       loader: 'akamai',
