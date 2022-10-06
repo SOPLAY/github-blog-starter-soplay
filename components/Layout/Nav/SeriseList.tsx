08cc7ee1a2b75d5caad3af7c8c2d54a86592223a
soplay <tkdgns1341@gmail.com>
2022-10-06 15:40:07 +0900

    Fix serises make logic

--- components/Layout/Nav/SeriseList.tsx
+++ components/Layout/Nav/SeriseList.tsx
@@ -9,28 +9,30 @@ const SeriseList = () => {
   const router = useRouter();
 
   return (
-    <div className='pt-12'>
-      <div className='w-[80%] mx-auto'>
-        <h3 className='pb-5 text-2xl font-semibold'>
-          <span className='relative inline-block before:block before:absolute before:-inset-1 before:-skew-y-3 before:to-base-gradient-from before:from-base-gradient-to before:bg-gradient-to-tr'>
-            <span className='relative text-white'>SeriseList</span>
-          </span>
-        </h3>
-        <div>
-          {serises.map((serise, index) => (
-            <div
-              key={index}
-              className={`${
-                router.query.serise === serise &&
-                'text-3xl font-bold duration-300 ease-out'
-              } py-[1px]`}
-            >
-              <Link href={`/post?serise=${serise}`}>{serise}</Link>
-            </div>
-          ))}
+    serises[0] && (
+      <div className='pt-12'>
+        <div className='w-[80%] mx-auto'>
+          <h3 className='pb-5 text-2xl font-semibold'>
+            <span className='relative inline-block before:block before:absolute before:-inset-1 before:-skew-y-3 before:to-base-gradient-from before:from-base-gradient-to before:bg-gradient-to-tr'>
+              <span className='relative text-white'>SeriseList</span>
+            </span>
+          </h3>
+          <div>
+            {serises.map((serise, index) => (
+              <div
+                key={index}
+                className={`${
+                  router.query.serise === serise &&
+                  'text-3xl font-bold duration-300 ease-out'
+                } py-[1px]`}
+              >
+                <Link href={`/post?serise=${serise}`}>{serise}</Link>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
