4c9b2c57a5afb3ea5d2c1ad9be4053ca98850a1e
soplay <tkdgns1341@gmail.com>
2022-10-10 19:28:55 +0900

    Add infinity load logic

--- pages/post.tsx
+++ pages/post.tsx
@@ -3,11 +3,12 @@ import Input from '@root/components/Input';
 import PostCard from '@root/components/PostCard';
 import _ from 'lodash';
 import { NextPage } from 'next';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Meta from '@root/components/Meta';
 import { title, description, url } from '@root/blog.config';
+import LoadingSpinner from 'public/assets/loading.svg';
 
 const PostPage: NextPage = () => {
   const [inputValue, setInputValue] = useState('');
@@ -56,31 +57,54 @@ const PostPage: NextPage = () => {
             )
           );
         }
-        setPosts(postList);
+        setPosts(_.orderBy(postList, ['date'], ['desc']));
       }, 200),
     [router]
   );
+  const loadRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entris) => {
+      entris[0].isIntersecting && setTargetPage(targetPage + 1);
+    });
+    if (loadRef.current) {
+      observer.observe(loadRef.current);
+    }
+    return () => observer.disconnect();
+  });
+  const [targetPage, setTargetPage] = useState(1);
 
   return (
     <>
       <Meta title={title} description={description} url={router.asPath} />
-      <div className='min-h-screen mx-auto mt-20 md:w-4/6'>
-        <div className='px-3 pt-5'>
-          <h1 className='py-10 text-6xl'>
-            <Link href={'/post'}>Post</Link>
-          </h1>
-          <div className='pb-10'>
-            <Input set={setInputValue} />
+      <div className='w-[90%] min-h-[87vh] pt-20 mx-auto h-fit '>
+        <div className='max-w-xl mx-auto'>
+          <div className='px-3 '>
+            <h1 className='pb-5 text-3xl font-bold'>
+              <Link href={'/post'}>Post</Link>
+            </h1>
+            <div className='pb-10 '>
+              <Input set={setInputValue} />
+            </div>
+          </div>
+          <div className={'flex flex-col  gap-2'}>
+            {posts.slice(0, targetPage * 6).map((value, index) => (
+              <PostCard
+                {...value}
+                key={index}
+                image={value.image ? fixFilePath(value.image, value) : ''}
+              />
+            ))}
           </div>
         </div>
-        {_.orderBy(posts, ['date'], ['desc']).map((value, index) => (
-          <PostCard
-            key={index}
-            {...value}
-            image={value.image ? fixFilePath(value.image, value) : ''}
-          />
-        ))}
       </div>
+      {targetPage * 6 < posts.length && (
+        <div className='' ref={loadRef}>
+          <div className='text-center'>
+            <LoadingSpinner />
+          </div>
+        </div>
+      )}
     </>
   );
 };
